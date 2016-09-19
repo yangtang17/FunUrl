73c11bce3717b4f2e82d7ecda1ecd44f21560a5e
Yang Tang <Yang Tang>
2016-09-18 23:59:22 -0500

    week1: create server code

---
+++ week1/server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var restRouter = require('./routes/rest');
+var redirectRouter = require('./routes/redirect');
+
+
+app.use("/api/v1", restRouter);
+
+app.use("/:shortUrl", redirectRouter);
+
+app.listen(8888);
---
+++ week1/services/urlService.js
@@ -0,0 +1,29 @@
+var longToShortHash = {};
+var shortToLongHash = {};
+
+var getShortUrl = function (longUrl) {
+    if (longUrl.indexOf('http') === -1) {
+        longUrl = 'http://' + longUrl;
+    }
+    if (longToShortHash[longUrl] != null) {
+        return longToShortHash[longUrl];
+    } else {
+        var shortUrl = generateShortUrl();
+        longToShortHash[longUrl] = shortUrl;
+        shortToLongHash[shortUrl] = longUrl;
+        return shortUrl;
+    }
+};
+
+var generateShortUrl = function () {
+    return Object.keys(longToShortHash).length;
+}
+
+var getLongUrl = function (shortUrl) {
+    return shortToLongHash[shortUrl];
+}
+
+module.exports = {
+    getShortUrl: getShortUrl,
+    getLongUrl: getLongUrl
+};
