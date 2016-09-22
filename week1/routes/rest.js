475c10b00f921814b2eb1dd5c2102c5d40aded98
Yang Tang <Yang Tang>
2016-09-21 23:29:21 -0500

    re-implement everything

--- week1/routes/rest.js
+++ week1/routes/rest.js
@@ -4,9 +4,8 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var urlService = require('../services/urlService');
 
-router.post("/urls", jsonParser, function (req, res) {
+router.post("/urls", jsonParser, function(req, res) {
     var longUrl = req.body.longUrl;
-    console.log(longUrl);
     var shortUrl = urlService.getShortUrl(longUrl);
     res.json({
         shortUrl: shortUrl,
--- week1/server.js
+++ week1/server.js
@@ -3,9 +3,9 @@ var app = express();
 var restRouter = require('./routes/rest');
 var redirectRouter = require('./routes/redirect');
 
-
 app.use("/api/v1", restRouter);
 
 app.use("/:shortUrl", redirectRouter);
 
-app.listen(8888);
+app.listen(8000);
+
