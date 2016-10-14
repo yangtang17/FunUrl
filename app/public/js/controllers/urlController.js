a8dfe2e4432049ab9ebfdb40155128ebf75fe4e7
Your Name <you@example.com>
2016-10-14 16:54:31 -0500

    add: display total clicks

--- app/public/js/controllers/urlController.js
+++ app/public/js/controllers/urlController.js
@@ -7,4 +7,9 @@ angular.module('tinyurlApp')
                 $scope.shortUrl = data.shortUrl;
                 $scope.shortUrlToShow = 'http://localhost:8000/' + data.shortUrl;
             });
+
+        $http.get('/api/v1/urls/' + $routeParams.shortUrl + '/totalClicks')
+            .success(function(data) {
+                $scope.totalClicks = data;
+            });
     }]);
--- app/routes/rest.js
+++ app/routes/rest.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var urlService = require('../services/urlService');
+var statsService = require('../services/statsService');
 
 // post a new longUrl
 router.post('/urls', jsonParser, function(req, res) {
@@ -17,6 +18,16 @@ router.get('/urls/:shortUrl', function(req, res) {
     var shortUrl = req.params.shortUrl;
     urlService.getLongUrl(shortUrl, function(url) {
         res.json(url);
+    });
+});
+
+// lookup shortUrl statistics
+router.get('/urls/:shortUrl/:info', function(req, res) {
+    var shortUrl = req.params.shortUrl;
+    var info = req.params.info;
+
+    statsService.getUrlInfo(shortUrl, info, function(data) {
+        res.json(data);
     });
 });
 
